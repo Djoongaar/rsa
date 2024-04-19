2072dd3c6a870e981e1e8ef4a6e67ff938c90f56
Evgeny Sharaev <e.sharaev@postgrespro.ru>
2024-04-19 23:10:08 +0800

    __encrypt_block method init

--- rsa.py
+++ rsa.py
@@ -1,18 +1,35 @@
+import math
+
+
 class RSA:
-    def __init__(self, public_key_path: str = None, private_key_path: str = None):
-        self.__public_key = self.__get_public_key(public_key_path)
-        self.__secret_key = self.__get_secret_key(public_key_path)
+    def __init__(self, public_key_path: str = None, secret_key_path: str = None):
+        self.__public_key = None
+        self.__secret_key = None
         self.__euler = None
-        self.__block_length = None
+        self.__module = None
+        self.__block_size = None
+        self.__get_keys(public_key_path, secret_key_path)
 
-    @staticmethod
-    def __get_public_key(public_key_path):
-        with open(public_key_path, 'r') as f:
-            return f.read()
+    def __get_keys(self, public_key_path, secret_key_path):
+        if public_key_path:
+            with open(public_key_path, 'r') as f:
+                self.__public_key = int(f.read().split("/")[0], 0)
+                self.__module = int(f.read().split("/")[1], 0)
 
-    @staticmethod
-    def __get_secret_key(secret_key_path):
-        with open(secret_key_path, 'r') as f:
-            return f.read()
+        if secret_key_path:
+            with open(secret_key_path, 'r') as f:
+                self.__secret_key = int(f.read().split("/")[0], 0)
+                self.__module = int(f.read().split("/")[1], 0)
 
+        if self.__module:
+            self.__block_size = int(math.log(self.__module, 2))
 
+    def __encrypt_block(self, block: bin):
+        block_integer = int(block, 2)
+        block_encrypt = pow(block_integer, self.__public_key, self.__module)
+        # Padding with zeros till byte size
+        bytes_count = (((self.__block_size + 1) // 8) + 1) * 8
+        return bin(block_encrypt)[2:].zfill(bytes_count)
+
+    def __str__(self):
+        return self.__public_key, self.__secret_key, self.__module, self.__block_size
