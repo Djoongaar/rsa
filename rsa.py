13d72e38e7b7643d48f4c043b753f3004f57efba
Evgeny Sharaev <e.sharaev@postgrespro.ru>
2024-04-22 13:40:15 +0800

    RSA is done

--- rsa.py
+++ rsa.py
@@ -12,6 +12,7 @@ class RSA:
         self.__module = None
         self.module_int = None
         self.__block_size = None
+        self.__block_size_full = None
         self.__get_keys(public_key_path, secret_key_path)
 
     def __get_keys(self, public_key_path, secret_key_path):
@@ -31,7 +32,8 @@ class RSA:
 
         if self.__module:
             self.module_int = int(self.__module, 0)
-            self.__block_size = int(math.log(self.module_int, 2))
+            self.__block_size = math.floor(math.log(self.module_int, 2))
+            self.__block_size_full = math.ceil(math.log(self.module_int, 2))
 
     @staticmethod
     def __read_file_as_binary(file_path: str) -> bytearray:
@@ -40,55 +42,82 @@ class RSA:
         return open_text
 
     @staticmethod
-    def __get_text_bin(open_text: bytearray):
-        result_bits = bin(0)
-        for i in range(len(open_text)):
-            result_bits += bin(int.from_bytes(open_text[i*8:(i+1)*8], byteorder="big", signed=False))[2:]
-        return result_bits
+    def __get_text_bin(open_text: bytearray) -> str:
+        return bin(int.from_bytes(open_text, byteorder="big", signed=False))
 
-    def encrypt_block(self, block: bin) -> bin:
+    def encrypt_block(self, block: str) -> str:
         block_integer = int(block, 2)
 
-        block_encrypt = pow(block_integer, self.public_key_int, self.module_int)
+        block_encrypt_int = pow(block_integer, self.public_key_int, self.module_int)
+        block_encrypt_bin = bin(block_encrypt_int)[2:].zfill(self.__block_size_full)
 
-        # Тут длинну блоков проверил - всё работает нормально
-        # print("RAW: ", len(block))
-        # print("ENC: ", len(bin(block_encrypt)) - 2)
+        return block_encrypt_bin
 
-        return bin(block_encrypt)
+    def decrypt_block(self, block: str) -> str:
+        block_integer = int(block, 2)
+
+        block_decrypt_int = pow(block_integer, self.secret_key_int, self.module_int)
+        block_decrypt_bin = bin(block_decrypt_int)[2:].zfill(self.__block_size)
+        return block_decrypt_bin
 
     def encrypt(self, path_to_file: str) -> bytearray:
         with open(path_to_file, mode="rb") as file:
             open_text = bytearray(file.read())
 
-        print(len(open_text), type(open_text))
+        open_text_bin = self.__get_text_bin(open_text)
+        result_bin = ""
 
-        open_text_bin = self.__get_text_bin(open_text)[2:]
-        result_bytes = bytearray()
+        blocks_count = math.ceil(len(open_text_bin) / self.__block_size)
 
-        for i in range(len(open_text_bin) // self.__block_size):
-            block_bin = open_text_bin[i * self.__block_size: (i + 1) * self.__block_size]
+        for i in range(1, blocks_count + 1):
+            start = -(i * self.__block_size)
+            end = -(i * self.__block_size - self.__block_size)
+
+            if not end:
+                end = None
+
+            block_bin = open_text_bin[start: end]
+
             enc_block_bin = self.encrypt_block(block_bin)
 
-            print(len(block_bin), type(block_bin))
+            result_bin = enc_block_bin + result_bin
 
-            enc_block_int = int(enc_block_bin, 2)
-            enc_block_bytes = int.to_bytes(enc_block_int, 128, byteorder="big", signed=False)
+        result_int = int(result_bin, 2)
 
-            print(len(enc_block_bytes), type(enc_block_bytes))
+        result_bytes = bytearray(int.to_bytes(result_int, length=512, byteorder="big", signed=False))
 
-            result_bytes.extend(enc_block_bytes)
+        with open("encrypted.txt", "wb") as file:
+            file.write(result_bytes)
 
         return result_bytes
 
     def decrypt(self, path_to_file: str):
-        encrypt_text = self.encrypt(path_to_file)
+        with open(path_to_file, "rb") as file:
+            cipher_text = bytearray(file.read())
 
-        result_bytes = bytearray()
+        cipher_text_bin = self.__get_text_bin(cipher_text)
 
-        for i in range(len(encrypt_text) // 8):
-            block = encrypt_text[i * self.__block_size: (i + 1) * self.__block_size]
-            # print(block)
+        result_bin = ""
+
+        blocks_count = math.ceil(len(cipher_text_bin) / self.__block_size_full)
+
+        for i in range(1, blocks_count + 1):
+            start = -(i * self.__block_size_full)
+            end = -(i * self.__block_size_full - self.__block_size_full)
+
+            if not end:
+                end = None
+
+            block_bin = cipher_text_bin[start: end]
+            decr_block_bin = self.decrypt_block(block_bin)
+
+            result_bin = decr_block_bin + result_bin
+
+        result_int = int(result_bin, 2)
+        result_bytes = bytearray(int.to_bytes(result_int, length=512, byteorder="big", signed=False))
+
+        with open("decrypted.txt", "wb") as file:
+            file.write(result_bytes)
 
         return result_bytes
 
@@ -100,5 +129,6 @@ class RSA:
             'secret_key_int': self.secret_key_int,
             'module': self.__module,
             'module_int': self.module_int,
-            'block_size': self.__block_size
+            'block_size': self.__block_size,
+            'block_size_full': self.__block_size_full
         }
